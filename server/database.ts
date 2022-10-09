b9be87615b2c1b03f119de467c674cda04227716
Arkenstone <trejo.allan@yahoo.com>
2022-10-08 21:06:40 -0700

    updated config files

---
+++ server/database.ts
@@ -0,0 +1,18 @@
+import * as express from 'express';
+import * as path from 'path';
+import * as http from 'http';
+import * as bodyParser from 'body-parser';
+
+class Server {
+  public app: express.Application;
+
+  public static bootstrap(): Server {
+    return new Server();
+  }
+
+  constructor () {
+    this.app = express();
+  }
+}
+
+Server.bootstrap();
