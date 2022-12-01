915d658799f3f953f13ec0975f18165e5f0ed2c5
Arkenstone <trejo.allan@yahoo.com>
2022-11-30 17:02:09 -0800

    added snackbar and error handling

--- server/controller/definition.controller.ts
+++ server/controller/definition.controller.ts
@@ -6,8 +6,6 @@ import capitalizeWord from '../utils/regexUtils';
 /* Gets  from dictionary */
 const getMatchingDefinitions = asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
   const wordCapitalzed = capitalizeWord(req.params[ 'word' ]);
-  console.log(wordCapitalzed);
-
   Definition.find({ word: wordCapitalzed }, (err: unknown, words: definition) => {
     if (err) {
       return next(err);
